00c18309db6ba36124535369f23b5a099764ca5c
Szécsi István <szecsi.istvan@gmail.com>
2022-06-21 16:05:33 +0200

    initial commit - project structure and hello world controller created

---
+++ treasury-auction-system/src/main/java/com/greenfox/treasuryauctionsystem/TreasuryAuctionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.treasuryauctionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasuryAuctionSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TreasuryAuctionSystemApplication.class, args);
+  }
+
+}
