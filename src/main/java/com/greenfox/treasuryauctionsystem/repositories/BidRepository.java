470ec8b307864e89c47df857d860ab47b2767b4b
Torda Péter <redflesher@gmail.com>
2022-06-23 16:26:40 +0200

    update test 1.3

--- src/main/java/com/greenfox/treasuryauctionsystem/repositories/BidRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenfox.treasuryauctionsystem.repositories;
-
-import com.greenfox.treasuryauctionsystem.models.Bid;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BidRepository extends JpaRepository<Bid,Long> {
-}
--- src/main/java/com/greenfox/treasuryauctionsystem/repositories/TreasurySecurityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenfox.treasuryauctionsystem.repositories;
-
-import com.greenfox.treasuryauctionsystem.models.TreasurySecurity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TreasurySecurityRepository extends JpaRepository<TreasurySecurity,Long> {
-}
