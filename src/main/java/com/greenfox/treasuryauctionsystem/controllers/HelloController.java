45de67cd252390899ff208f20af38b8957bf35ef
Torda Péter <redflesher@gmail.com>
2022-06-23 16:02:08 +0200

    update test 1.0

--- src/main/java/com/greenfox/treasuryauctionsystem/controllers/HelloController.java
+++ src/main/java/com/greenfox/treasuryauctionsystem/controllers/HelloController.java
@@ -2,16 +2,14 @@ package com.greenfox.treasuryauctionsystem.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
 
-  @GetMapping("hello")
-  @ResponseBody
-  public String helloController(){
-    return "Hello world!";
-  }
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helloController() {
+		return "Hello world!";
+	}
 }
