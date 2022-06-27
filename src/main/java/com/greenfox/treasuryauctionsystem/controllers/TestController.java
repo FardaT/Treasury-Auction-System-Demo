f9f2b02a0566ad57f1b7c82cea2cbdd3e275801f
FardaT <96318104+FardaT@users.noreply.github.com>
2022-06-27 23:42:06 +0200

    NMTAS-1 Amended with test page and controller for authorization

---
+++ src/main/java/com/greenfox/treasuryauctionsystem/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.greenfox.treasuryauctionsystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+  @GetMapping("/test")
+  public String testController(){
+    return "test";
+  }
+
+}
