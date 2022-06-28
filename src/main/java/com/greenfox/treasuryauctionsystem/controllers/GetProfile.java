8541462218d9887852c45916ea19857c3024c4c3
Torda Péter <redflesher@gmail.com>
2022-06-29 01:54:13 +0200

    get profile endpoint implemented

--- src/main/java/com/greenfox/treasuryauctionsystem/controllers/GetProfile.java
+++ src/main/java/com/greenfox/treasuryauctionsystem/controllers/GetProfile.java
@@ -1,10 +1,12 @@
 package com.greenfox.treasuryauctionsystem.controllers;
 
+import com.greenfox.treasuryauctionsystem.models.AppUser;
 import com.greenfox.treasuryauctionsystem.services.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GetProfile {
@@ -17,8 +19,9 @@ public class GetProfile {
 	}
 
 	@GetMapping("/profile")
-	public String getProfile (Model model){
-		model.addAttribute("username",);
+	public String getProfile (@RequestParam String userName, Model model){
+		AppUser user = appUserService.findUserByUsername(userName);
+		model.addAttribute("user", user);
 		return "profile";
 	}
 
