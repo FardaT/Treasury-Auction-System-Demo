0fda3450cfe4e08f4f2655cb268b88a7e7b3e0df
David Lukacs <lukacs.dvid@gmail.com>
2022-06-29 16:16:53 +0200

    test case response successful, spring sec permits all requests

--- src/main/java/com/greenfox/treasuryauctionsystem/security/SecurityConfig.java
+++ src/main/java/com/greenfox/treasuryauctionsystem/security/SecurityConfig.java
@@ -35,31 +35,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/register").permitAll()
-                .antMatchers("/confirm").permitAll()
-                .antMatchers("/confirm_token").permitAll()
-                .antMatchers("/confirm_token/**").permitAll()
-                .antMatchers("/store").permitAll()
-                .antMatchers("/resetpassword").permitAll()
-                .antMatchers("/resetpassword/**").permitAll()
-                .antMatchers("/login")
-                .permitAll().and()
-                .formLogin()
-                .loginPage("/login").and()
-                .logout()
-                .deleteCookies("jwtoken")
-                .logoutSuccessUrl("/login").and()
-                .authorizeRequests().anyRequest().authenticated().and()
-                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
-                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-
 //        http.csrf().disable();
 //        http.sessionManagement().sessionCreationPolicy(STATELESS);
-//        http.authorizeRequests().anyRequest().permitAll();
+//        http.authorizeRequests()
+//                .antMatchers("/admin/**").hasRole("ADMIN")
+//                .antMatchers("/register").permitAll()
+//                .antMatchers("/confirm").permitAll()
+//                .antMatchers("/confirm_token").permitAll()
+//                .antMatchers("/confirm_token/**").permitAll()
+//                .antMatchers("/store").permitAll()
+//                .antMatchers("/resetpassword").permitAll()
+//                .antMatchers("/resetpassword/**").permitAll()
+//                .antMatchers("/login")
+//                .permitAll().and()
+//                .formLogin()
+//                .loginPage("/login").and()
+//                .logout()
+//                .deleteCookies("jwtoken")
+//                .logoutSuccessUrl("/login").and()
+//                .authorizeRequests().anyRequest().authenticated().and()
+//                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
+//                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
 
     }
 
