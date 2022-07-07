18975ac15a506bb4d341c33293aad75be552ee8f
David Lukacs <lukacs.dvid@gmail.com>
2022-07-07 12:01:49 +0200

    config values added to app properties file

--- src/main/java/com/greenfox/treasuryauctionsystem/services/BidServiceImpl.java
+++ src/main/java/com/greenfox/treasuryauctionsystem/services/BidServiceImpl.java
@@ -2,6 +2,7 @@ package com.greenfox.treasuryauctionsystem.services;
 
 import com.greenfox.treasuryauctionsystem.models.Bid;
 import com.greenfox.treasuryauctionsystem.repositories.BidRepository;
+import com.greenfox.treasuryauctionsystem.utils.ApplicationDetails;
 import com.greenfox.treasuryauctionsystem.utils.Utility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,23 +38,23 @@ public class BidServiceImpl implements BidService {
         if (bid.getAmount() <= 0) {
             errors.put("AMOUNT_POSITIVE", "Amount has to be a positive number!");
         }
-        if (bid.getAmount() % 100 != 0) {
+        if (bid.getAmount() % ApplicationDetails.multiple_of != 0) {
             errors.put("AMOUNT_HUNDRED", "Amount has to be the multiple of hundred!");
         }
 
         // Competitive bidding is limited to 35% of the offering amount for each bidder
         if(bid.isCompetitive()) {
-            if(bid.getAmount() > bid.getTreasurySecurity().getTotalAmount() * 0.35) {
+            if(bid.getAmount() > bid.getTreasurySecurity().getTotalAmount() * ApplicationDetails.percentage) {
                 errors.put("AMOUNT_COMPETITIVE", "Competitive bidding is limited to 35% of the offering amount for each bidder!");
             }
         } else {
             // Noncompetitive bidding is limited to purchases of $5 million per auction
-            if(bid.getAmount() > 5000000) {
+            if(bid.getAmount() > ApplicationDetails.max_amount) {
                 errors.put("AMOUNT_NONCOMPETITIVE", "Noncompetitive bidding is limited to purchases of $5 million per auction!");
             }
         }
 
-        if (bid.getRate() <= 0 || bid.getRate() > 10) {
+        if (bid.getRate() <= ApplicationDetails.min_rate || bid.getRate() > ApplicationDetails.max_rate) {
             errors.put("RATE_RANGE", "Rate has to be between 0 and 10!");
         }
 
--- src/main/java/com/greenfox/treasuryauctionsystem/utils/ApplicationDetails.java
+++ src/main/java/com/greenfox/treasuryauctionsystem/utils/ApplicationDetails.java
@@ -9,10 +9,33 @@ public class ApplicationDetails {
     // https://stackoverflow.com/questions/25764459/spring-boot-application-properties-value-not-populating
 
     public static int expiration;
+    public static float percentage;
+    public static long max_amount;
+    public static int multiple_of;
+    public static float min_rate;
+    public static float max_rate;
 
     @Value("${expiration}")
-    public void setProp(int expiration) {
-        ApplicationDetails.expiration = expiration;
+    public void setExpiration(int expiration) {ApplicationDetails.expiration = expiration;}
+    @Value("${percentage}")
+    public void setPercentage(float percentage) {
+        ApplicationDetails.percentage = percentage;
+    }
+    @Value("${max_amount}")
+    public void setMax_amount(long max_amount) {
+        ApplicationDetails.max_amount = max_amount;
+    }
+    @Value("${multiple_of}")
+    public void setMultiple_of(int multiple_of) {
+        ApplicationDetails.multiple_of = multiple_of;
+    }
+    @Value("${min_rate}")
+    public void setMin_rate(float min_rate) {
+        ApplicationDetails.min_rate = min_rate;
+    }
+    @Value("${max_rate}")
+    public void setMax_rate(float max_rate) {
+        ApplicationDetails.max_rate = max_rate;
     }
 
     public ApplicationDetails() {
