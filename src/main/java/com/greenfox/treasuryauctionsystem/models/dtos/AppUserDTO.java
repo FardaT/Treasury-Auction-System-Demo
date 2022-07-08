f9659c25ed5cbc1e1c804a3d0f5880423f60743b
Peter Torda <62599671+redflesher@users.noreply.github.com>
2022-07-08 13:19:43 +0200

    create AppUserDTO.java

---
+++ src/main/java/com/greenfox/treasuryauctionsystem/models/dtos/AppUserDTO.java
@@ -0,0 +1,26 @@
+package com.greenfox.treasuryauctionsystem.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class AppUserDTO {
+
+	private String username;
+	private String email;
+	private List<BidDTO> bids = new ArrayList<>();
+	private String institution;
+	private boolean isAdmin;
+	private boolean isActivated;
+	private boolean isApproved;
+	private boolean isDisabled;
+
+}
