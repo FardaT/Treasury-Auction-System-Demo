7be4c35f2e94d398976180813626d31843bfb810
David Lukacs <lukacs.dvid@gmail.com>
2022-07-11 11:13:14 +0200

    auction id added to bids list and we have two separate SAs now

--- src/main/resources/static/js/admin/SA_dialog.js
+++ src/main/resources/static/js/admin/SA_dialog.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    // ADMIN/USERS
     $(".sa_click").click(function (e) {
 
         // alert("The paragraph was clicked.");
@@ -26,5 +28,24 @@ $(document).ready(function () {
             }
         })
 
+    });
+
+    // AUCTIONLIST
+    $(".sa_click_twoButtons").click(function (e) {
+        e.preventDefault();
+        var form = $(this).parents('form');
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, proceed!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit();
+            }
+        })
     });
 });
