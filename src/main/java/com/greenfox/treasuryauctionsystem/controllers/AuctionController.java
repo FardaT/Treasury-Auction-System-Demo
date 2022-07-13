931f5f0c112c8bc9c9a7b36907257239001fb924
Szécsi István <szecsi.istvan@gmail.com>
2022-07-13 19:59:30 +0200

    minor issue solved

--- src/main/java/com/greenfox/treasuryauctionsystem/controllers/AuctionController.java
+++ src/main/java/com/greenfox/treasuryauctionsystem/controllers/AuctionController.java
@@ -1,8 +1,7 @@
 package com.greenfox.treasuryauctionsystem.controllers;
 
-import com.greenfox.treasuryauctionsystem.models.AppUser;
-
 import com.greenfox.treasuryauctionsystem.exceptions.NoSuchAuctionException;
+import com.greenfox.treasuryauctionsystem.models.AppUser;
 import com.greenfox.treasuryauctionsystem.models.dtos.AuctionResponseDTO;
 import com.greenfox.treasuryauctionsystem.services.AppUserService;
 import com.greenfox.treasuryauctionsystem.services.AuctionService;
@@ -22,33 +21,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuctionController {
 
 
-    private final AuctionService auctionService;
-    private final AppUserService appUserService;
+  private final AuctionService auctionService;
+  private final AppUserService appUserService;
 
-    @Autowired
-    public AuctionController(AuctionService auctionService, AppUserService appUserService) {
-        this.auctionService = auctionService;
-        this.appUserService = appUserService;
-    }
+  @Autowired
+  public AuctionController(AuctionService auctionService, AppUserService appUserService) {
+    this.auctionService = auctionService;
+    this.appUserService = appUserService;
+  }
 
-    @GetMapping()
-    public String showAllAuctions(Model model, Principal principal) {
+  @GetMapping()
+  public String showAllAuctions(Model model, Principal principal) {
 
-        // get currently logged in user
-        AppUser currentUser = appUserService.getUserByUsername(principal.getName());
-        model.addAttribute("user", currentUser);
+    // get currently logged in user
+    AppUser currentUser = appUserService.getUserByUsername(principal.getName());
+    model.addAttribute("user", currentUser);
 
-        Map<String, List<AuctionResponseDTO>> auctions = auctionService.getAllAuctionsByStatus();
+    Map<String, List<AuctionResponseDTO>> auctions = auctionService.getAllAuctionsByStatus();
+    model.addAttribute("auctions", auctions);
 
+    return "/admin/auctionslist";
+  }
 
-        return "/admin/auctionslist";
-    }
-
-    @PostMapping("/disable")
-    public String deleteAuctionPost(@RequestParam Long id) {
-        auctionService.disable(id);
-        return "redirect:/auctions";
-    }
+  @PostMapping("/disable")
+  public String deleteAuctionPost(@RequestParam Long id) {
+    auctionService.disable(id);
+    return "redirect:/auctions";
+  }
 
 //    @GetMapping("/process/{id}")
 //    public String processFinishedAuction(@PathVariable Long id) {
